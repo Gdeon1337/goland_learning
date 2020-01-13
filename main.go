64202f8babc6a8ffd965de8870e2a6c8164b8930
Gdeon1337 <gdeom1337@gmail.com>
2020-01-13 13:17:14 +0400

    add logger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"./app"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -12,6 +13,7 @@ import (
 func task() {
 	timerCh := time.Tick(time.Duration(60) * time.Second)
 	for range timerCh {
+		log.Print("Start task - update currencies")
 		app.CurrencyParser()
 	}
 }
@@ -24,22 +26,17 @@ func main() {
 
 	router.HandleFunc("/api/user",
 		app.CreateUser).Methods("POST")
-
 	router.HandleFunc("/api/user/login",
 		app.Authenticate).Methods("POST")
 
 	router.HandleFunc("/api/currency",
 		app.CreateCurrencies).Methods("POST")
-
 	router.HandleFunc("/api/currency",
 		app.UpdateCurrencies).Methods("PUT")
-
 	router.HandleFunc("/api/currency",
 		app.DeleteCurrencies).Methods("DELETE")
-
 	router.HandleFunc("/api/currency/convert",
 		app.ConvertCurrency).Methods("GET")
-
 	router.HandleFunc("/api/currency",
 		app.GetCurrencies).Methods("GET")
 
